24743bb427918af78b6737d478be25a7fb18f622
Ademola Osindero <ademola@lopworks.com>
2014-10-27 00:01:11 +0000

    Update search model to use basic search

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -10,6 +10,8 @@ class TicketTest < ActiveSupport::TestCase
     ticket = tickets(:one)
     ticket.save
     assert_match "Unassigned", ticket.status 
+    
+    ticket.send(:set_reference)
     ref_reg = /^[A-Z]{3}-\h\h-[A-Z]{3}-\h\h-[A-Z]{3}$/i
     refute_nil ref_reg.match(ticket.ref)
   end
